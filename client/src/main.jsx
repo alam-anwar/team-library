8001a76786e9b448eb8df7dc5dbcc589efa417d1
rschneeberger1 <rschneeberger@ufl.edu>
2024-07-10 19:55:50 -0400

    Added Create Button

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -40,6 +40,16 @@ const router = createBrowserRouter([
       },
     ],
   },
+  {
+    path: "/create",
+    element: <App />,
+    children: [
+      {
+        path: "/create",
+        element: <Record />,
+      },
+    ],
+  },
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
