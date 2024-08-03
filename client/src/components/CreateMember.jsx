9ce30ad9b22e92dd77f84f634adc437e65ef7ad0
rschneeberger1 <rschneeberger@ufl.edu>
2024-08-03 16:12:07 -0400

    Cleaned up createMember

--- client/src/components/CreateMember.jsx
+++ client/src/components/CreateMember.jsx
@@ -36,6 +36,61 @@ export default function CreateMember() {
         onSubmit={onSubmit}
         className="border rounded-lg overflow-hidden p-4"
       >
+        <div class="form-group">
+          <label for="username">Username</label>
+          <input
+            type="text"
+            name="username"
+            id="username"
+            placeholder="Enter username"
+            value={form.username}
+            onChange={(e) => updateForm({ username: e.target.value })}
+          />
+        </div>
+        <div class="form-group">
+          <label for="password">Password</label>
+          <input
+            type="password"
+            name="password"
+            id="password"
+            placeholder="Enter password"
+            value={form.password}
+            onChange={(e) => updateForm({ password: e.target.value })}
+          />
+        </div>
+        <div class="form-group">
+          <label for="phone">Phone Number</label>
+          <input
+            type="text"
+            name="phone"
+            id="phone"
+            placeholder="Enter phone number"
+            rows="4"
+            value={form.phone}
+            onChange={(e) => updateForm({ phone: e.target.value })}
+          />
+        </div>
+        <div class="form-group">
+          <label for="email">Email</label>
+          <input
+            type="text"
+            name="email"
+            id="email"
+            placeholder="Enter email"
+            value={form.email}
+            onChange={(e) => updateForm({ email: e.target.value })}
+          />
+        </div>
+        <input
+          type="submit"
+          value="Save Item"
+          className="inline-flex items-center justify-center font-medium border h-9 rounded-md px-3 cursor-pointer mt-4 border-gray-400"
+        />
+      </form>
+      <form
+        onSubmit={onSubmit}
+        className="border rounded-lg overflow-hidden p-4"
+      >
         <div className="grid grid-cols-1 gap-x-8 gap-y-10 border-b border-slate-900/10 pb-12 md:grid-cols-2">
           <div>
             <h2 className="list-none list-inside leading-10 text-base font-semibold text-slate-900 text-right">
