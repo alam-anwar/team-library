4c39cfdc2cad73e540d1ca699a7fc0117623c511
rschneeberger1 <rschneeberger@ufl.edu>
2024-08-06 12:07:12 -0400

    Touched up events and inventory pages

--- client/src/components/EventList.jsx
+++ client/src/components/EventList.jsx
@@ -17,7 +17,7 @@ const Event = (props) => (
             className="inline-flex items-center justify-center whitespace-nowrap text-md font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border border-input bg-background hover:bg-slate-100 h-9 rounded-md px-3 mr-2"
             to={`./rsvp/${props.event._id}`}
           >
-            Info
+            Details
           </Link>
         </div>
       </div>
